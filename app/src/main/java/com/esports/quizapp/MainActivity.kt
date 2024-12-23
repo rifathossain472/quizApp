f9add47313340ad849f75ed82e12e66db26ff885
Rifat Hossain <mdrifathossain472@gmail.com>
2024-12-24 02:13:52 +0600

    initial commit

---
+++ app/src/main/java/com/esports/quizapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.esports.quizapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.esports.quizapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
